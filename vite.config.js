648ddfbe929376b29b423b736d92ce9e5ba28cdf
ln549283 <loic.nebonne@gmail.com>
2023-03-17 15:04:34 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+       host: 'localhost', 
+  },
   plugins: [
     vue({ 
       template: { transformAssetUrls }
